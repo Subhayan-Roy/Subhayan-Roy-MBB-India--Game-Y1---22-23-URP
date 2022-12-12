eeff383d592273c0af5b5afb474014c352b34776
Ian <58906578+Ian-Tc@users.noreply.github.com>
2022-12-12 17:39:58 +0530

    Vfx Explosive Brick example + code to trigger it

--- GDY1 MBB 2022-23/Assets/Scripts/MBB/GameObjects/BrickScripts/ExplosiveBrick.cs
+++ GDY1 MBB 2022-23/Assets/Scripts/MBB/GameObjects/BrickScripts/ExplosiveBrick.cs
@@ -7,19 +7,27 @@ public class ExplosiveBrick : BrickBase
     public float explosionRadius = 1;
     public LayerMask brickLayer;
     public int explosiveBrickDamage = 10;
+	public Transform vfxTransform;
 
     Collider2D[] bricksInRadius;
 
     bool hasExploded;
 
-    protected override void Dead()
+	protected override void Awake()
+	{
+		base.Awake();
+
+		vfxTransform.parent = null; //We make the vfx independent so that it won't get destroyed when the brick gets destroyed
+	}
+
+	protected override void Dead()
     {
         if (!hasExploded)
         {
             hasExploded = true;
             Explode();
             gameManager.BrickHasDied();
-            Destroy(this.gameObject);
+			Destroy(gameObject);
         }
         
     }
@@ -36,6 +44,8 @@ public class ExplosiveBrick : BrickBase
                 bb.Damage(explosiveBrickDamage);
             }
         }
+
+		vfxTransform.gameObject.SetActive(true); //We activate the vfx object so that it plays its stuff
     }
 
     void GetBricksToExplode()
